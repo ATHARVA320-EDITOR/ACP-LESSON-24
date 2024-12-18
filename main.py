761e012ae2ec74082780c7a267f5298effc898f3
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2024-12-18 21:11:37 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+numbers = [24,35,38,32,91,104]
+odd = [a for a in numbers if a%2!=0]
+print("Odd numbers in the list are: ",odd)
+numbers2 = [45,23,58,60,87,104]
+even = [a for a in numbers if a%2==0]
+print("Odd numbers in the list are: ",even)
