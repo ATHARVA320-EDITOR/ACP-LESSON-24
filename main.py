e8b2e13e306f156b85eafeebd9fc62bb0866969e
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2024-12-21 15:52:15 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-numbers = [24,35,38,32,91,104]
-odd = [a for a in numbers if a%2!=0]
+number = int(input("Enter a number: "))
+number2 = int(input("Enter 2nd number: "))
+number3= int(input("Enter 3rd number: "))
+number4 = int(input("Enter 4th number: "))
+list = [number, number2, number3, number4]
+print("The list is:" ,list)
+odd = [a for a in list if a%2!=0]
 print("Odd numbers in the list are: ",odd)
-numbers2 = [45,23,58,60,87,104]
-even = [a for a in numbers if a%2==0]
-print("Odd numbers in the list are: ",even)
